1807c4cf3ce1f704dc8954ad7816371d1bb24ab8
carlosavanzini <avanzinicarlos@gmail.com>
2022-08-08 20:49:31 -0300

    tercer commit

--- javascript.js
+++ javascript.js
@@ -4,10 +4,10 @@
     // alert('Hola a todos los Rollings')
 
 /*
-2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (console.log).
+2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
 
 */
-    // console.log('Hola a todos desde consola')
+    document.write('Hola a todos desde html')
 
 /*
 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
